efb3a96f891d1d89fd7d4bdaf1ef5a7318cdb135
Tyler <phantomxbla@gmail.com>
2022-04-10 16:04:47 -0400

    Started experimenting with mouse input as rotation method / slider likely the way to go

--- Assets/[Scripts]/DialBehaviour.cs
+++ Assets/[Scripts]/DialBehaviour.cs
@@ -8,14 +8,51 @@ public class DialBehaviour : MonoBehaviour
     [SerializeField]
     public GameObject handle;
     public Slider knobSlider;
-
+    Vector3 mousePos;
     private readonly float rotationLimit = 22.5f;
 
+    Vector3 mousePressPoint;
     public void RotateHandle()
     {
-        handle.transform.localEulerAngles = new Vector3(0, knobSlider.value * rotationLimit, 0);
+        //handle.transform.localEulerAngles = new Vector3(0, knobSlider.value * rotationLimit, 0);
+        handle.transform.localEulerAngles = new Vector3(0, (mousePos.x * rotationLimit), 0);
 
-        
+        Mathf.Clamp(handle.transform.localEulerAngles.y, 0, 202.5f);
 
+    }
+
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        //Debug.Log(handle.transform.localEulerAngles);
+        //if(handle.transform.localEulerAngles.y >= 0 && handle.transform.localEulerAngles.y <= 202.5)
+        //{
+
+        //}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePressPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RotateHandle();
+        }
+
+       
+       
+
+
+    }
+
+    void OnMouseDrag()
+    {
+        RotateHandle();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("yes");
     }
 }
